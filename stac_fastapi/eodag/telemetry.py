4033f9131ce22d0c28804ee2686260559a76dd4e
Camille Auriol <camille.auriol@cs-soprasteria.com>
2025-06-06 16:25:01 +0200

    fix: linter deprecated

--- stac_fastapi/eodag/telemetry.py
+++ stac_fastapi/eodag/telemetry.py
@@ -33,9 +33,10 @@ from opentelemetry.sdk.metrics._internal.aggregation import (
 )
 from opentelemetry.sdk.metrics._internal.export import PeriodicExportingMetricReader
 from opentelemetry.sdk.metrics._internal.view import View
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.semconv.resource import ResourceAttributes
 
 if TYPE_CHECKING:
     from fastapi import FastAPI
@@ -128,7 +129,7 @@ def instrument_fastapi(
 ) -> None:
     """Instrument FastAPI app."""
     logger.info("Instrument FastAPI app")
-    resource = Resource(attributes={SERVICE_NAME: "stac-fastapi-eodag"})
+    resource = Resource(attributes={ResourceAttributes.SERVICE_NAME: "stac-fastapi-eodag"})
     tracer_provider = create_tracer_provider(resource)
     meter_provider = create_meter_provider(resource)
     FastAPIInstrumentor.instrument_app(
@@ -141,7 +142,7 @@ def instrument_fastapi(
 def instrument_eodag(eodag_api: EODataAccessGateway):
     """Instrument EODAG app"""
     logger.info("Instrument EODAG app")
-    resource = Resource(attributes={SERVICE_NAME: "stac-fastapi-eodag"})
+    resource = Resource(attributes={ResourceAttributes.SERVICE_NAME: "stac-fastapi-eodag"})
     tracer_provider = create_tracer_provider(resource)
     meter_provider = create_meter_provider(resource)
     EODAGInstrumentor(eodag_api).instrument(
