674c009d5d7ea885db9c5c2412266a015611303d
Camille Auriol <camille.auriol@cs-soprasteria.com>
2025-12-03 17:15:20 +0100

    fix: remove test with numbermatched

--- tests/test_search.py
+++ tests/test_search.py
@@ -533,7 +533,6 @@ async def test_ids_post_search(request_valid, defaults):
 async def test_search_response_contains_pagination_info(request_valid, defaults):
     """Responses to valid search requests must return a geojson with pagination info in properties"""
     response = await request_valid(f"search?collections={defaults.collection}")
-    assert "numberMatched" in response
     assert "numberReturned" in response
 
 
